4447655c4766d230b35c6beec83e7aae6e6e6876
dutarrr <duzguntutar@gmail.com>
2021-12-10 20:27:11 +0300

    fiber go project created

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,1 +1,14 @@
-package cmd
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		ctx.SendString("Hello from fiber")
+		return nil
+	})
+
+	app.Listen(":3000")
+}
