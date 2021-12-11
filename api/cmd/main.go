bcd2b6ed10e901e1982f66bcc804c9088d3aa902
dutarrr <duzguntutar@gmail.com>
2021-12-11 16:02:08 +0300

    mongo db connection is done

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
 
 func main() {
+	client, ctx, cancel, err := connectMongo("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	defer closeMongo(client, ctx, cancel)
+
+	pingMongo(client, ctx)
+
 	app := fiber.New()
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
@@ -11,4 +28,33 @@ func main() {
 	})
 
 	app.Listen(":3000")
+}
+
+func connectMongo(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+
+	return client, ctx, cancel, err
+}
+
+func pingMongo(client *mongo.Client, ctx context.Context) error {
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("mongo baglantisi basarili..")
+	return nil
+}
+
+func closeMongo(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+
+	defer cancel()
+
+	defer func() {
+
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
